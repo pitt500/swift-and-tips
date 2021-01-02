afb21a4c49cc3c3cfc23cb4f95723c733a5ef2ae
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-01 18:54:59 -0700

    Initial project for Environment object demo

---
+++ EnvironmentObjectDemo/EnvironmentObjectDemo/EnvironmentObjectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectDemoApp.swift
+//  EnvironmentObjectDemo
+//
+//  Created by Pedro Rojas on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
