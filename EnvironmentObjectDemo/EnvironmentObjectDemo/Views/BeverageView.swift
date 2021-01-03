917ef9a678902fe09cd001907398a193472755f6
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-03 15:32:05 -0700

    Add Food, Beverage and Dessert Views

---
+++ EnvironmentObjectDemo/EnvironmentObjectDemo/Views/BeverageView.swift
@@ -0,0 +1,20 @@
+//
+//  BeverageView.swift
+//  EnvironmentObjectDemo
+//
+//  Created by Pedro Rojas on 1/3/21.
+//
+
+import SwiftUI
+
+struct BeverageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BeverageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeverageView()
+    }
+}
---
+++ EnvironmentObjectDemo/EnvironmentObjectDemo/Views/DessertView.swift
@@ -0,0 +1,20 @@
+//
+//  DessertView.swift
+//  EnvironmentObjectDemo
+//
+//  Created by Pedro Rojas on 1/3/21.
+//
+
+import SwiftUI
+
+struct DessertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DessertView_Previews: PreviewProvider {
+    static var previews: some View {
+        DessertView()
+    }
+}
