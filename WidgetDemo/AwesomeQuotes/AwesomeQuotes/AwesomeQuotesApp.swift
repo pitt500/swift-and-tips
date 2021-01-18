8e0e519759322e8e92411e5808e2892aa4de558d
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-18 09:39:04 -0700

    Add Widget Demo

---
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/AwesomeQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeQuotesApp.swift
+//  AwesomeQuotes
+//
+//  Created by Pedro Rojas on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwesomeQuotes
+//
+//  Created by Pedro Rojas on 1/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
