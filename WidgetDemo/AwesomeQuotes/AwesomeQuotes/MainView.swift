30b8e54828dd1c1e0f720bbacb0ea88f30175b6e
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-18 21:37:11 -0700

    Rename to MainView

--- WidgetDemo/AwesomeQuotes/AwesomeQuotes/MainView.swift
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/MainView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
