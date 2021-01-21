d7c429e9e7152b1b797270009ac032f7d8bea1d8
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-21 01:20:57 -0700

    Configure list for deeplink

--- WidgetDemo/AwesomeQuotes/AwesomeQuotes/Helpers/NavigationLinkNoArrow.swift
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/Helpers/NavigationLinkNoArrow.swift
@@ -7,28 +7,30 @@
 
 import SwiftUI
 
-public struct NavigationLinkNoArrow<Content: View, Destination: View>: View {
+public struct NavigationLinkNoArrow<Content: View, Destination: View, V: Hashable>: View {
     
     private var destination: Destination
     private var content: Content
-    @State private var isActive = false
+    @Binding private var selected: V?
+    private let tag: V
+    
     
     public init(
         destination: @autoclosure @escaping () -> Destination,
-        content: @autoclosure @escaping () -> Content
+        content: @autoclosure @escaping () -> Content,
+        selected: Binding<V?>,
+        tag: V
     ) {
         self.destination = destination()
         self.content = content()
+        self._selected = selected
+        self.tag = tag
     }
     
     public var body: some View {
-        Button(action: { self.isActive = true }) {
-            self.content
-                .background(
-                    NavigationLink(destination: destination, isActive: self.$isActive) {
-                        EmptyView()
-                    }.hidden()
-                )
-        }
+        self.content
+            .background(
+                NavigationLink(destination: destination, tag: tag, selection: $selected, label: { EmptyView() })
+            )
     }
 }
--- WidgetDemo/AwesomeQuotes/AwesomeQuotes/Views/MainView.swift
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/Views/MainView.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct MainView: View {
     
+    @State private var selected: UUID?
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(Quote.sample) { quote in
                     NavigationLinkNoArrow (
                         destination: QuoteDetail(quote: quote),
-                        content: QuoteCell(quote: quote)
+                        content: QuoteCell(quote: quote),
+                        selected: $selected,
+                        tag: quote.id
                     )
                     .listRowInsets(EdgeInsets())
                 }
             }
+            .onOpenURL { url in
+                guard let index = Quote.sample.firstIndex (where: { $0.url == url })
+                else { return }
+                
+                selected = Quote.sample[index].id
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
             .navigationTitle("Awesome Quotes")
         }
     }
