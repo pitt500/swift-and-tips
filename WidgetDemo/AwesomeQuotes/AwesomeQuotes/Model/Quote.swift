9926af0c4e3aa38ebfd3763cb0b0bc30a6092b57
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-21 01:20:20 -0700

    Remove unused elements and remove random images

--- WidgetDemo/AwesomeQuotes/AwesomeQuotes/Model/Quote.swift
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/Model/Quote.swift
@@ -8,79 +8,50 @@
 import Foundation
 
 struct Quote: Identifiable {
-    let id = UUID().uuidString
+    let id = UUID()
     let description: String
     let author: String
     let profession: String
-    let randomImageValue = Int.random(in: 1...6)
+    let imageNumber: Int
+    let url: URL
 }
 
 extension Quote {
-    static let sample = [
-        Quote(
-            description: "Keep your face to the sunshine and you cannot see a shadow.",
-            author: "Helen Keller",
-            profession: "Author"
-        ),
+    static var sample = [
         Quote(
             description: "No one is perfect - that’s why pencils have erasers.",
             author: "Wolfgang Riebe",
-            profession: "Keynote Speaker-Magician"
+            profession: "Keynote Speaker-Magician",
+            imageNumber: 1,
+            url: URL(string: "awesomeQuotes:///quote2")!
         ),
         Quote(
             description: "You always pass failure on the way to success.",
             author: "Mickey Rooney",
-            profession: "Actor"
-        ),
-        Quote(
-            description: "You’re off to great places, today is your day. Your mountain is waiting, so get on your way.",
-            author: "Dr. Seuss",
-            profession: "Author-Poet"
+            profession: "Actor",
+            imageNumber: 2,
+            url: URL(string: "awesomeQuotes:///quote3")!
         ),
         Quote(
             description: "It always seems impossible until it is done.",
             author: "Nelson Mandela",
-            profession: "Political Leader"
-        ),
-        Quote(
-            description: "You’re braver than you believe, and stronger than you seem, and smarter than you think.",
-            author: "A.A. Mine",
-            profession: "Author-Poet"
-        ),
-        Quote(
-            description: "Once you replace negative thoughts with positive ones, you’ll start having positive results.",
-            author: "Willie Nelson",
-            profession: "Musician"
-        ),
-        Quote(
-            description: "The only time you fail is when you fall down and stay down.",
-            author: "Stephen Richards",
-            profession: "Author"
+            profession: "Political Leader",
+            imageNumber: 3,
+            url: URL(string: "awesomeQuotes:///quote5")!
         ),
         Quote(
             description: "If opportunity doesn’t knock, build a door.",
             author: "Milton Berle",
-            profession: "Comedian"
+            profession: "Comedian",
+            imageNumber: 4,
+            url: URL(string: "awesomeQuotes:///quote9")!
         ),
         Quote(
             description: "Success is the sum of small efforts repeated day in and day out.",
             author: "Robert Collier",
-            profession: "Author"
-        ),
-        Quote(
-            description: "The good life is a process, not a state of being. It is a direction, not a destination.",
-            author: "Carl Rogers",
-            profession: "Psychologist"
-        ),
-        Quote(
-            description: "Live life to the fullest and focus on the positive.",
-            author: "Matt Cameron",
-            profession: "Musician"
-        ),
-        Quote(
-            description: "So long King Bowser!!",
-            author: "Mario Bros.",
-            profession: "Plumber"
+            profession: "Author",
+            imageNumber: 5,
+            url: URL(string: "awesomeQuotes:///quote10")!
         ),
     ]
 }
