49660116a58cd6b4ba04b4c37d3f867de8c403df
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-23 00:46:04 -0700

    Rename preview

--- WidgetDemo/AwesomeQuotes/AwesomeQuotes/Views/MainView.swift
+++ WidgetDemo/AwesomeQuotes/AwesomeQuotes/Views/MainView.swift
@@ -35,7 +35,7 @@ struct MainView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
     }
