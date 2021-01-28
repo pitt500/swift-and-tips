d0db1d754e52c46af5ae1e948414fab6c1502343
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-28 07:27:11 -0700

    Initial Project

---
+++ ViewCompositionDemo/ViewCompositionDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewCompositionDemo
+//
+//  Created by Pedro Rojas on 28/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewCompositionDemo/ViewCompositionDemo/ViewCompositionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewCompositionDemoApp.swift
+//  ViewCompositionDemo
+//
+//  Created by Pedro Rojas on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ViewCompositionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
