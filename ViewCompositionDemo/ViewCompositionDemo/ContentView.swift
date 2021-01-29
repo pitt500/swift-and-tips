73abdc3ef02f1e93276c6f1aa1055275fefdb85f
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-28 23:06:57 -0700

    Add stacks to build spotify player

--- ViewCompositionDemo/ViewCompositionDemo/ContentView.swift
+++ ViewCompositionDemo/ViewCompositionDemo/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     let isEmpty = false
+    @State private var progress: Float = 0.5
     
     var body: some View {
         ZStack {
             Color.black
-            .ignoresSafeArea()
+                .ignoresSafeArea()
             LinearGradient(
                 gradient: Gradient(
                     colors: [
-                        Color.pink.opacity(0.7),
-                        Color.pink.opacity(0.4)
+                        Color.pink.opacity(0.6),
+                        Color.pink.opacity(0.3)
                     ]
                 ),
                 startPoint: .top,
@@ -26,13 +27,141 @@ struct ContentView: View {
             )
             .ignoresSafeArea()
             
-            VStack {
+            VStack(alignment: .center) {
+
+                HStack(alignment: .top) {
+                    Image(systemName: "chevron.down")
+                        .foregroundColor(.white)
+                        .font(
+                            .system(
+                                size: 20,
+                                weight: .regular,
+                                design: .default
+                            )
+                        )
+
+                    Spacer()
+                    Text("Queen")
+                        .foregroundColor(.white)
+                        .font(
+                            .system(
+                                size: 15,
+                                weight: .bold,
+                                design: .default
+                            )
+                        )
+                    Spacer()
+                    Image(systemName: "ellipsis")
+                        .foregroundColor(.white)
+                        .font(
+                            .system(
+                                size: 20,
+                                weight: .bold,
+                                design: .default
+                            )
+                        )
+
+                }
+                .frame(minHeight: 0, maxHeight: 100)
+
+
+                Spacer()
+
                 Image("queen")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                Text("Bohemian Rhapsody")
-            }
+                    .frame(minHeight: 150, maxHeight: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.bottom, 30)
+                
+                VStack (alignment: .leading) {
+                    Text("Bohemian Rhapsody")
+                        .foregroundColor(.white)
+                        .font(
+                            .system(
+                                size: 30,
+                                weight: .bold,
+                                design: .default
+                            )
+                        )
+
+                    Text("Queen")
+                        .foregroundColor(Color.white.opacity(0.8))
+                        .font(.callout)
+
+                    ZStack(alignment: .leading){
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                            .frame(height: 5)
+
+                        Capsule()
+                            .fill(Color.white)
+                            .frame(width: 150, height: 5)
+
+                        Circle()
+                            .fill(Color.white)
+                            .shadow(radius: 10)
+                            .frame(width: 15, height: 15)
+                            .offset(x: 150)
+                        //.gesture(DragGesture().onChanged(30))
+                    }
+
+                    HStack {
+                        Text("0:00")
+                        Spacer()
+                        Text("-5:53")
+                    }
+                    .foregroundColor(.white)
+                    .font(.caption)
+
+                    HStack {
+                        Image(systemName: "shuffle")
+                            .foregroundColor(Color.white.opacity(0.7))
+                        Spacer()
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "backward.end.fill")
+                                    .resizable()
+                                    .foregroundColor(.white)
+                                    .frame(width: 30, height: 30)
+                            }
+                        )
+                        Spacer()
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "play.circle.fill")
+                                    .resizable()
+                                    .foregroundColor(.white)
+                                    .frame(width: 80, height: 80)
+                            }
+                        )
+                        Spacer()
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "forward.end.fill")
+                                    .resizable()
+                                    .foregroundColor(.white)
+                                    .frame(width: 30, height: 30)
+                            }
+                        )
+                        Spacer()
+                        Image(systemName: "repeat")
+                            .foregroundColor(Color.white.opacity(0.7))
+                    }
+
+                    Spacer()
+                    HStack (alignment: .bottom){
+                        Image(systemName: "tv.and.hifispeaker.fill")
+                        Spacer()
+                        Image(systemName: "list.triangle")
+                    }
+                    .foregroundColor(.white)
+
+
+                }
+            }.padding([.leading, .trailing, .bottom])
         }
     }
 }
