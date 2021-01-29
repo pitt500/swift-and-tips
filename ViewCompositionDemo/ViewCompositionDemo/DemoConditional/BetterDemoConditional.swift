88bdc752826e08a75887a66d13a0f289d7075cd1
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-01-29 07:30:55 -0700

    Add Demo Conditional

---
+++ ViewCompositionDemo/ViewCompositionDemo/DemoConditional/BetterDemoConditional.swift
@@ -0,0 +1,20 @@
+//
+//  BetterDemoConditional.swift
+//  ViewCompositionDemo
+//
+//  Created by Pedro Rojas on 29/01/21.
+//
+
+import SwiftUI
+
+struct BetterDemoConditional: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BetterDemoConditional_Previews: PreviewProvider {
+    static var previews: some View {
+        BetterDemoConditional()
+    }
+}
