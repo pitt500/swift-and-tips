14e2ae54c0326edfefd4d9bb5c14c6fb62c4d612
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-02-15 08:20:03 -0700

    Add UIViewReprentable Demo

---
+++ UIViewRepresentableDemo/UIViewRepresentableDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIViewRepresentableDemo
+//
+//  Created by Pedro Rojas on 15/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIViewRepresentableDemo/UIViewRepresentableDemo/UIViewRepresentableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableDemoApp.swift
+//  UIViewRepresentableDemo
+//
+//  Created by Pedro Rojas on 15/02/21.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
