b98c19acf12e9d0fa6bbafd7cc030b87d0f9f69b
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-03-23 07:24:48 -0700

    Add demo in SwiftUI

---
+++ Generics/Generics_SwiftUI/Generics_SwiftUI/Generics_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Generics_SwiftUIApp.swift
+//  Generics_SwiftUI
+//
+//  Created by Pedro Rojas on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Generics_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
