b54d2b95342c39932309cee94a3486065411ab9e
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-04-07 07:10:59 -0700

    Add opaque type demo for SwiftUI

---
+++ OpaqueTypes/OpaqueTypes_SwiftUI/OpaqueTypes_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OpaqueTypes_SwiftUI
+//
+//  Created by Pedro Rojas on 07/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpaqueTypes/OpaqueTypes_SwiftUI/OpaqueTypes_SwiftUI/OpaqueTypes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpaqueTypes_SwiftUIApp.swift
+//  OpaqueTypes_SwiftUI
+//
+//  Created by Pedro Rojas on 07/04/21.
+//
+
+import SwiftUI
+
+@main
+struct OpaqueTypes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
