525f89a3b65dc3cbe21d29f54b4233681f8fdd35
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-04 06:28:53 -0700

    Add Combine intro demo

---
+++ CombineIntroDemo/CombineIntroDemo/CombineIntroDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineIntroDemoApp.swift
+//  CombineIntroDemo
+//
+//  Created by Pedro Rojas on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CombineIntroDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
