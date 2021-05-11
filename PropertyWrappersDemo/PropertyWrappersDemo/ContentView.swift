667a4b13c5cd28c81442aec8c7278457d48898f2
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-11 06:43:47 -0700

    Add Demo using nested state

--- PropertyWrappersDemo/PropertyWrappersDemo/ContentView.swift
+++ PropertyWrappersDemo/PropertyWrappersDemo/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ReinventedState var text = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(text)
+            TextField("hello", text: $text)
+                .disableAutocorrection(true)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+@propertyWrapper
+struct ReinventedState: DynamicProperty {
+    @State private var value: String
+
+    init(wrappedValue: String) {
+        self._value = State(wrappedValue: wrappedValue)
+    }
+
+    var wrappedValue: String {
+        get { value }
+        nonmutating set {
+            value = newValue
+                .uppercased()
+        }
+    }
+
+    var projectedValue: Binding<String> {
+        Binding(
+            get: { wrappedValue },
+            set: { wrappedValue = $0 }
+        )
     }
 }
