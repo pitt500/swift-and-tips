d5a24277fae5c58e04403aab4be0505c9bdb3e04
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-11 21:15:51 -0700

    Add demo 1 about age wrapper

---
+++ PropertyWrappersDemo/PropertyWrappersPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+struct User {
+    var email: String
+    @Age var age: Int
+    var firstName: String
+    var lastName: String
+}
+
+@propertyWrapper
+struct Age {
+    private var age: Int
+
+    init(wrappedValue initialValue: Int) {
+        self.age = max(0, initialValue)
+    }
+
+    var wrappedValue: Int {
+        get { age }
+        set { age = max(0, newValue) }
+    }
+}
+
+
+var user = User(
+    email: "hello@demo.com",
+    age: -1,
+    firstName: "Pitt",
+    lastName: "Rojas"
+)
+
+// Initial value
+print(user.age) // output: 0
+
+//Modifying the value
+user.age = -1000
+print(user.age) // output: 0
+
+
+//Correct age
+user.age = 34
+print(user.age) // output: 34
