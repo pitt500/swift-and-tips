5d82dcf50338e071f0e18b9b1a5a340c00dea042
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-16 11:53:32 -0700

    Add property wrapper demo with DynamicProperty protocol

---
+++ PropertyWrappersDynamicProperty/PropertyWrappersDemo/PropertyWrappersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersDemoApp.swift
+//  PropertyWrappersDemo
+//
+//  Created by Pedro Rojas on 11/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
