b0dd2a1f8fdd4b5c69b0d9db39108a77f302ae7b
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-25 07:32:17 -0700

    Add demo for trailing closures

--- Closures/ClosuresDemo.playground/Pages/What is an incorrect way to use trailing closures?.xcplaygroundpage/Contents.swift
+++ Closures/ClosuresDemo.playground/Pages/What is an incorrect way to use trailing closures?.xcplaygroundpage/Contents.swift
@@ -2,6 +2,66 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+enum DemoError: Error {
+    case randomError
+}
+
+func someProcess(completion: () -> Void, failure: (DemoError) -> Void) {
+    let isEverythingOk = Bool.random()
+
+    if isEverythingOk {
+        completion()
+    } else {
+        failure(.randomError)
+    }
+}
+
+func someProcess(completion: () -> Void) {
+    completion()
+}
+
+/// Trailing closure is a special syntax for closures at the end of your function that can be written (for the last one) at
+/// the end of function's parenthesis.
+
+// MARK: - A ✅
+//(Recommended by https://github.com/raywenderlich/swift-style-guide#closure-expressions)
+someProcess(
+    completion: {
+        print("Success!")
+    },
+    failure: { error in
+        print("Error: \(error)")
+    }
+)
+
+// MARK: - B
+someProcess {
+    print("Success!")
+} failure: { error in
+    print("Error: \(error)")
+}
+
+// MARK: - C
+someProcess(
+    completion: {
+        print("Success!")
+    }
+) { error in
+    print("Error: \(error)")
+}
+
+// MARK: - D ✅
+//(Recommended by https://github.com/raywenderlich/swift-style-guide#closure-expressions)
+someProcess {
+    print("Success!")
+}
+
+// MARK: - E
+someProcess(completion: {
+    print("Success!")
+})
+
+
+
 
 //: [Next](@next)
