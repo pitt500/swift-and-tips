40bb0d1a0fdfd8c3652ebbaaa2962e8512949054
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-25 07:12:55 -0700

    Add demo for minimum expression closure

--- Closures/ClosuresDemo.playground/Pages/What is the minimum expression?.xcplaygroundpage/Contents.swift
+++ Closures/ClosuresDemo.playground/Pages/What is the minimum expression?.xcplaygroundpage/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// Full closure expression
+[1, 2, 3].map(
+    { (value: Int) -> String in
+        return String(value)
+    }
+)
+
+// value's type can be inferred
+[1, 2, 3].map(
+    { value -> String in
+        return String(value)
+    }
+)
+
+// closure's return type can be inferred
+[1, 2, 3].map(
+    { value in
+        return String(value)
+    }
+)
+
+// shorthand notation for arguments.
+[1, 2, 3].map({ return String($0) })
+
+
+// For single line expression, return can be omitted
+[1, 2, 3].map({ String($0) })
+
+
