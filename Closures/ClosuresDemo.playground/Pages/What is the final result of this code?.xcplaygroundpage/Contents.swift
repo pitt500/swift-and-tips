f54284caa0d08075ff69d37354b7da86df5f56ad
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-25 11:07:17 -0700

    Add capturing demo

--- Closures/ClosuresDemo.playground/Pages/What is the final result of this code?.xcplaygroundpage/Contents.swift
+++ Closures/ClosuresDemo.playground/Pages/What is the final result of this code?.xcplaygroundpage/Contents.swift
@@ -2,6 +2,24 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+
+func executeOperation() -> (Int) -> Int {
+    var value = 0
+
+    return { anotherValue in
+        value += anotherValue
+        return value
+    }
+}
+
+let operation = executeOperation()
+let x = operation(10)
+let y = operation(12)
+let z = operation(8)
+
+print(z)
+
+
 
 //: [Next](@next)
