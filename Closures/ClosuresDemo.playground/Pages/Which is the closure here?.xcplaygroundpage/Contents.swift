97d26ba939332c7706a7d4cb61d073930f91207a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-25 07:03:20 -0700

    Add What is the closure demo

--- Closures/ClosuresDemo.playground/Pages/Which is the closure here?.xcplaygroundpage/Contents.swift
+++ Closures/ClosuresDemo.playground/Pages/Which is the closure here?.xcplaygroundpage/Contents.swift
@@ -2,6 +2,43 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+
+
+
+// MARK: - A
+func sayHello(to name: String) {
+    print("Hello \(name)!")
+}
+sayHello(to: "Swift and Tips")
+
+
+// MARK: - B
+func calculateAverage(_ array: [Int]) -> Int {
+    var result = 0
+
+    func average() -> Int {
+        for value in array {
+            result += value
+        }
+
+        return result/array.count
+    }
+
+    return average()
+}
+print(calculateAverage([1, 2, 3]))
+
+
+// MARK: - C
+let add = { (a: Int, b: Int) in
+    return a + b
+}
+print(add(3,2))
+
+
+
+
+
 
 //: [Next](@next)
