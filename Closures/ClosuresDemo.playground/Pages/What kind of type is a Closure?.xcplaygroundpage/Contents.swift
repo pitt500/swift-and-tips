6c8bed33aac3e7d2b8a6a11324e892defd34af69
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-05-25 11:18:50 -0700

    add demo for closure type

--- Closures/ClosuresDemo.playground/Pages/What kind of type is a Closure?.xcplaygroundpage/Contents.swift
+++ Closures/ClosuresDemo.playground/Pages/What kind of type is a Closure?.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+func combineWords() -> (String) -> String {
+    var string = ""
+
+    return { word in
+        string += word
+        return string
+    }
+}
+
+// Closures are reference types 💡
+let x: (String) -> String = combineWords()
+let y = x
+
+let resultX = x("Hello")
+let resultY = y("Goodbye")
+
+print(resultY)
+
 
 //: [Next](@next)
