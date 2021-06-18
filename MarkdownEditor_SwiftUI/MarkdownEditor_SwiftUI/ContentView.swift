04c57bae33b6196d4dd2067db73e62fb345146dd
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-18 05:52:29 -0700

    Change preview to landscape

--- MarkdownEditor_SwiftUI/MarkdownEditor_SwiftUI/ContentView.swift
+++ MarkdownEditor_SwiftUI/MarkdownEditor_SwiftUI/ContentView.swift
@@ -17,5 +17,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
