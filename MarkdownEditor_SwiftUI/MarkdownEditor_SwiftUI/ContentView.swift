a9b204d1b7bf943b9f39285c996e6aff3242fd57
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-18 12:32:01 -0700

    Add line break for markdown editor

--- MarkdownEditor_SwiftUI/MarkdownEditor_SwiftUI/ContentView.swift
+++ MarkdownEditor_SwiftUI/MarkdownEditor_SwiftUI/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var text = "hello"
+    @State private var text = "hello \n world"
 
     init() {
         UITextView.appearance().backgroundColor = .clear
@@ -17,24 +17,51 @@ struct ContentView: View {
     var body: some View {
         HStack {
             TextEditor(text: $text)
+                .disableAutocorrection(true)
                 .font(
                     .custom(
-                        "SF Mono",
-                        size: 20,
+                        "Arial",
+                        size: 25,
                         relativeTo: .body
                     )
                 )
                 .background(.blue)
                 .foregroundColor(.white)
-            Text(text)
-                .background(.green)
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: .infinity,
-                    alignment: .topLeading
-                )
+            ScrollView {
+                VStack {
+                    Text(attributedText)
+                        .lineLimit(nil)
+                        .font(
+                            .custom(
+                                "Arial",
+                                size: 25,
+                                relativeTo: .body
+                            )
+                        )
+                        .background(.green)
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity,
+                            alignment: .topLeading
+                        )
+                }.frame(maxWidth: .infinity)
+            }
+
+
         }
         .padding()
+    }
+
+    var attributedText: AttributedString {
+
+        let text = text.replacingOccurrences(of: "\n", with: "\\\n")
+
+        guard let attributedText = try? AttributedString(markdown: text)
+        else {
+            return AttributedString("Error Parsing text")
+        }
+
+        return attributedText
     }
 }
 
@@ -46,5 +73,65 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
+
+
+struct LongText: View {
+
+    /* Indicates whether the user want to see all the text or not. */
+    @State private var expanded: Bool = true
+
+    /* Indicates whether the text has been truncated in its display. */
+    @State private var truncated: Bool = false
+
+    private var text: AttributedString
+
+    var lineLimit = 3
+
+    init(_ text: AttributedString) {
+        self.text = text
+    }
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            // Render the real text (which might or might not be limited)
+            Text(text)
+                .lineLimit(expanded ? nil : lineLimit)
+
+                .background(
+
+                    // Render the limited text and measure its size
+                    Text(text).lineLimit(lineLimit)
+                        .background(GeometryReader { displayedGeometry in
+
+                            // Create a ZStack with unbounded height to allow the inner Text as much
+                            // height as it likes, but no extra width.
+                            ZStack {
+
+                                // Render the text without restrictions and measure its size
+                                Text(self.text)
+                                    .background(GeometryReader { fullGeometry in
+
+                                        // And compare the two
+                                        Color.clear.onAppear {
+                                            self.truncated = fullGeometry.size.height > displayedGeometry.size.height
+                                        }
+                                    })
+                            }
+                            .frame(height: .greatestFiniteMagnitude)
+                        })
+                        .hidden() // Hide the background
+            )
+
+            if truncated { toggleButton }
+        }
+    }
+
+    var toggleButton: some View {
+        Button(action: { self.expanded.toggle() }) {
+            Text(self.expanded ? "Show less" : "Show more")
+                .font(.caption)
+        }
     }
 }
