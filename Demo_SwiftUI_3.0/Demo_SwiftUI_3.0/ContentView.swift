565b9f9263ac7b85e9bc5f6f19e861f947a687e0
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-19 15:03:31 -0700

    Fix issue with AsyncImage (workaround)

--- Demo_SwiftUI_3.0/Demo_SwiftUI_3.0/ContentView.swift
+++ Demo_SwiftUI_3.0/Demo_SwiftUI_3.0/ContentView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    let url = URL(string: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg")
+    @StateObject private var loader = PokemonLoader()
+
     var body: some View {
-        AsyncImage(
-            url: url,
-            content: { $0.resizable() },
-            placeholder: { Text("Placeholder") }
-        )
-        .frame(width: 300, height: 300)
+        NavigationView {
+            List {
+                LazyVStack {
+                    ForEach(loader.pokemonData) { pokemon in
+                        AsyncImage(url: pokemon.url, scale: 1.0) { phase in
+                            if let image = phase.image {
+
+                                HStack {
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 120)
+                                        .padding(.trailing, 10)
+                                    VStack(alignment: .leading) {
+                                        Text("**\(pokemon.name.capitalized)**")
+                                            .font(.system(size: 24))
+                                        Text("*#\(pokemon.id)*")
+                                            .font(.system(size: 18))
+                                            .foregroundColor(.gray)
+
+                                        // Markdown links only work with string literals.
+                                        Text("See [More](https://pokemondb.net/pokedex/national)")
+                                        // You cannot use string interpolation ...
+//                                        Text("See [More](https://pokemondb.net/pokedex/\(pokemon.name))")
+                                    }
+                                    Spacer()
+                                }
+                                .frame(height: 130)
+                            } else if phase.error != nil {
+                                ErrorView(error: phase.error!)
+                            } else {
+                                Text("Loading")
+                            }
+                        }
+
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                    }
+
+
+                }
+            }
+            .navigationTitle("Hello")
+            .task {
+                loader.load()
+            }
+            .refreshable {
+                loader.load()
+            }
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ErrorView: View {
+    let error: Error
+
+    var body: some View {
+        print(error)
+        return Text("Error")
     }
 }
---
+++ Demo_SwiftUI_3.0/Demo_SwiftUI_3.0/PokemonLoader.swift
@@ -0,0 +1,68 @@
+//
+//  PokemonLoader.swift
+//  Demo_SwiftUI_3.0
+//
+//  Created by Pedro Rojas on 19/06/21.
+//
+
+import Combine
+import Foundation
+
+enum PokemonError: Error, Identifiable {
+    case serverError
+    case noData
+
+    var id: Self {
+        self
+    }
+}
+
+class PokemonLoader: ObservableObject {
+    @Published var pokemonData: [Pokemon] = []
+    @Published var isLoading = false
+    @Published var error = false
+
+    private var cancellable: AnyCancellable?
+    private let urlSession = URLSession(configuration: .default)
+    private let limit = 3
+    private var offset = 0
+    private var delta = 3
+
+    func load() {
+        isLoading = true
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)&offset=\(offset)")!
+
+        cancellable = urlSession
+            .dataTaskPublisher(for: url)
+            .tryMap { output in
+                guard let response = output.response as? HTTPURLResponse, response.statusCode == 200 else {
+                    throw PokemonError.serverError
+                }
+                return output.data
+            }
+            .decode(type: PokemonResponse.self, decoder: JSONDecoder())
+            .map(\.results)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                self.isLoading = false
+                switch completion {
+                case .finished:
+                    self.error = false
+                    break
+                case .failure(let error):
+                    print(error)
+                    self.error = true
+                }
+            },receiveValue: { [weak self] response in
+                guard let self = self else { return }
+                print(response.map(\.name))
+                self.pokemonData = response.reversed() + self.pokemonData
+                //self.pokemonData.append(contentsOf: response)
+                self.offset += self.delta
+            })
+    }
+}
+
+
+
