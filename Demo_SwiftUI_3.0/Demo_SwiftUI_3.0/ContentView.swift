724c0c44f6df1f557f8a3172c78011e3be8f373b
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-19 06:31:42 -0700

    Add new SwiftUI demo 3.0

---
+++ Demo_SwiftUI_3.0/Demo_SwiftUI_3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo_SwiftUI_3.0
+//
+//  Created by Pedro Rojas on 19/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo_SwiftUI_3.0/Demo_SwiftUI_3.0/Demo_SwiftUI_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUI_3_0App.swift
+//  Demo_SwiftUI_3.0
+//
+//  Created by Pedro Rojas on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUI_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
