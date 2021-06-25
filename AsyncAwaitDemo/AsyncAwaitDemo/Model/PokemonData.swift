d0e90a97edeb01117796af120dd43865f0abf10c
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-25 06:35:13 -0700

    Add Pokemon loader and model

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/Model/PokemonData.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct PokemonResponse: Decodable {
+    let count: Int
+    let next: String?
+    let previous: String?
+    let results: [Pokemon]
+}
+
+struct Pokemon: Decodable, Identifiable {
+    private static var totalFound = 0
+
+    let id: Int
+    let name: String
+    var  url: URL {
+        URL(string: "https://img.pokemondb.net/artwork/large/\(name).jpg")!
+    }
+
+    private enum PokemonKeys: String, CodingKey {
+        case name
+    }
+
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PokemonKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        Pokemon.totalFound += 1
+        self.id = Pokemon.totalFound
+    }
+}
+
+extension Pokemon {
+    static let sample = Self.init(id: 1, name: "bulbasaur")
+}
+
