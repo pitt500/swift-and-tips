295a6dbf5dd84b58956263fecc99f0cc9bc20967
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-06-25 06:20:10 -0700

    Add AsyncAwait demo

---
+++ AsyncAwaitDemo/AsyncAwaitDemo/AsyncAwaitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitDemoApp.swift
+//  AsyncAwaitDemo
+//
+//  Created by Pedro Rojas on 25/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
