f4b52c6ca555e30b6599fe2f30f7895dda0f7fe3
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-07-02 06:28:37 -0700

    Add Dismiss demo

--- AlertSwiftUI/AlertSwiftUI/ContentView.swift
+++ AlertSwiftUI/AlertSwiftUI/ContentView.swift
@@ -7,10 +7,79 @@
 
 import SwiftUI
 
+struct User {
+    let name: String
+    let age: Int
+}
+
+enum DemoError: LocalizedError {
+    case randomError
+
+    var errorDescription: String? {
+        return "Demo Error happened"
+    }
+}
+
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var user = User(name: "Pitt", age: 25)
+    @State private var error = DemoError.randomError
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let _ = Self._printChanges()
+        Button("Tap to show alert") {
+            showAlert = true
+        }
+//        .alert(
+//            "Hello", isPresented: $showAlert, presenting: user
+//        ) { user in
+//            Button(role: .destructive) {
+//                print("Delete it!")
+//            } label: {
+//                Text("Delete \(user.name)")
+//            }
+//            Button("Retry") {
+//                print("Retry action")
+//            }
+//        } message: { user in
+//            Text("My age is \(user.age)")
+//        }
+        .alert("Demo", isPresented: $showAlert, actions: {}, message: { Text("description") })
+
+//        .alert(isPresented: $showAlert) {
+//            Alert(
+//                title: Text("Current Location Not Available"),
+//                message: Text("Your current location can’t be " +
+//                              "determined at this time.")
+//            )
+//        }
+//        .alert(isPresented: $showAlert, error: error) { error in
+//            Button("OK") {
+//                print("ok error")
+//            }
+//        } message: { error in
+//            Text("Error loading your content")
+//        }
+
+//        .alert(
+//            "Hello", isPresented: $showAlert, presenting: user
+//        ) { user in
+//            Button("Default", role: .none, action: { print("Hello1") })
+//            Button("Second", role: .cancel, action: { print("Hello2") })
+//            Button("Destroy", role: .destructive, action: { print("Hello3") })
+//        } message: { user in
+//            Text(user.name)
+//        }
+
+//        .alert(isPresented: $showAlert) {
+//            Alert(
+//                title: Text("Current Location Not Available"),
+//                message: Text("Your current location can’t be " +
+//                                "determined at this time.")
+//            )
+//        }
+
+
     }
 }
 
---
+++ DismissSwiftUI/DismissSwiftUI/DismissSwiftUIApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct DismissSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
