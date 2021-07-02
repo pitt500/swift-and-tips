6befe92a60b5fe31e2ecc5e7d57a048ae24dd041
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-07-02 06:26:41 -0700

    Add alert modifier demo

---
+++ AlertSwiftUI/AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by Pedro Rojas on 01/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertSwiftUI/AlertSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlertSwiftUI
+//
+//  Created by Pedro Rojas on 01/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
