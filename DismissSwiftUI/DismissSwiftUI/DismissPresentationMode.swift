1308b41c78f3f07f12c06fb33ec43d79619a905e
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-07-02 16:42:12 -0700

    Add DismissPresentationMode

---
+++ DismissSwiftUI/DismissSwiftUI/DismissPresentationMode.swift
@@ -0,0 +1,40 @@
+
+import SwiftUI
+
+struct DismissPresentationMode: View {
+    @State private var showModal = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Show Sheet") {
+                    showModal.toggle()
+                }
+            }
+            .sheet(isPresented: $showModal) {
+                print("View Dismissed")
+            } content: {
+                ModalView()
+            }
+
+
+        }
+    }
+}
+
+struct ModalView: View {
+    @Environment(\.presentationMode) private var presentationMode
+
+    var body: some View {
+        Button("Press to dismiss") {
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DismissPresentationMode()
+    }
+}
+
