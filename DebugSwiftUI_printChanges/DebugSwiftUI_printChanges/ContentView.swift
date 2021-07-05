a7e9b1be546d24a7dda03bb5ee13172a246e12e4
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-07-05 06:00:15 -0700

    Add printChanges demo

---
+++ DebugSwiftUI_printChanges/DebugSwiftUI_printChanges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DebugSwiftUI_printChanges
+//
+//  Created by Pedro Rojas on 05/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DebugSwiftUI_printChanges/DebugSwiftUI_printChanges/DebugSwiftUI_printChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebugSwiftUI_printChangesApp.swift
+//  DebugSwiftUI_printChanges
+//
+//  Created by Pedro Rojas on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct DebugSwiftUI_printChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
