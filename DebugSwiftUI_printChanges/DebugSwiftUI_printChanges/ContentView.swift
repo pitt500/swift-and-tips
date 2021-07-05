a64eada1bb6b27d49f99ffe7e309a35a7baf72da
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-07-05 07:47:24 -0700

    Add form with state properties for _printChanges demo

--- DebugSwiftUI_printChanges/DebugSwiftUI_printChanges/ContentView.swift
+++ DebugSwiftUI_printChanges/DebugSwiftUI_printChanges/ContentView.swift
@@ -7,10 +7,88 @@
 
 import SwiftUI
 
+enum FrequencyNotification: String, CaseIterable {
+    case always = "Always"
+    case onlyWhenUnlocked = "When Unlocked"
+    case never = "Never"
+}
+
 struct ContentView: View {
+    @State private var notificationEnabled = true
+    @State private var frequency = FrequencyNotification.always
+    @State private var description = ""
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let _ = Self._printChanges()
+
+        /// In this example, frequency is not producing a new render because
+        /// is not been called in the view, however, if you simply uncomment the print
+        /// below, you will see that now view is rendered again, because frequency
+        /// variable is changing in the view's body 🤓!
+        //print(frequency)
+
+        return NavigationView {
+            Form {
+
+                //MARK: - Profile
+                Section {
+                    HStack {
+                        Image("Pitt")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        VStack(alignment: .leading) {
+                            Text("Pitt")
+                                .font(.largeTitle.bold())
+                            Text("Swift && Tips")
+                                .font(.caption.italic())
+                        }
+                    }
+
+                } header: {
+                    Text("Profile")
+                }
+
+                //MARK: - Description
+                Section {
+                    TextField(
+                        "Enter your description here",
+                        text: $description
+                    )
+                } header: {
+                    Text("Description")
+                }
+
+                //MARK: - Notifications
+                Section {
+                    Toggle(isOn: $notificationEnabled) {
+                        Text("Enable Notifications")
+                    }
+
+                    Picker(selection: $frequency, label: Text("Show Previews")) {
+                        ForEach(FrequencyNotification.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                    .disabled(notificationEnabled == false)
+
+                } header: {
+                    Text("Notifications")
+                }
+
+                //MARK: - About
+                Section {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("1.0.1")
+                    }
+                } header: {
+                    Text("About")
+                }
+
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
