94939b886ac1b38cab813cd25f86b8e9368298b1
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-08-03 07:58:13 -0700

    Add subscript demo

---
+++ Subscripts.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let birds = ["Articuno", "Zapdos", "Moltres"]
+
+let dateOfBirth = [
+    "Maradona" : 1960,
+    "Pele": 1940,
+    "Cruyff": 1947,
+    "Puyol": 1978
+]
+
+
+print(birds[1])
+print(dateOfBirth["Puyol"]!)
