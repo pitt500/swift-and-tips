2765fdc0724b9112c9a7d709448a0267a9d717b1
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-08-03 21:41:24 -0700

    Add example with safe array and verbose methods

---
+++ Subscripts/Subscripts.playground/Sources/Support.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// ONLY FOR DEMO PURPOSES!
+extension Array {
+    func getValue(at index: Int) -> Element {
+        // ... find the element
+        return self[index]
+    }
+}
+
+extension Dictionary {
+    func getValue(for key: Key) -> Value? {
+        // ... find the value
+        return self[key]
+    }
+}
