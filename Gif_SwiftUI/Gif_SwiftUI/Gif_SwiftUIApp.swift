f21eeb7ab0da04c25e1d1848a6b8eda5c0c8af3d
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-08-15 08:57:33 -0700

    Add Git SwiftUI project

---
+++ Gif_SwiftUI/Gif_SwiftUI/Gif_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gif_SwiftUIApp.swift
+//  Gif_SwiftUI
+//
+//  Created by Pedro Rojas on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Gif_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
