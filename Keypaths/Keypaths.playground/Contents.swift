f4eebd49cce11377143ce30f0036e3632cb1797a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-09-11 06:44:29 -0700

    Add other examples of keypaths

--- Keypaths/Keypaths.playground/Contents.swift
+++ Keypaths/Keypaths.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 // MARK: - Problems that Key Path solves
 
 struct Person {
-    let name: String
+    var name: String
     let age: Int
 }
 
@@ -45,5 +45,87 @@ struct ContentView: View {
                 Text("\(name)")
             }
         }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// MARK: - Types of key paths
+
+func uppercased<T>(object: inout T, keypath: WritableKeyPath<T, String>) {
+    object[keyPath: keypath] = object[keyPath: keypath].uppercased()
+}
+
+uppercased(object: &person1, keypath: \Person.name)
+print(person1.name)
+
+var aCEO = "Tim Apple"
+
+uppercased(object: &aCEO, keypath: \String.self)
+print(aCEO)
+
+
+
+
+// MARK: - ReferenceWritableKeypath
+
+class AnotherPerson {
+    var name: String
+    let age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+var another = AnotherPerson(name: "Bill", age: 50)
+
+let structKeypath = \Person.name
+let classKeyPath = \AnotherPerson.name
+
+uppercased(object: &another, keypath: classKeyPath)
+print(another.name)
+
+
+
+
+
+
+
+
+
+
+// MARK: - PartialKeyPath
+
+let keyPathName = \Person.name
+let keyPathAge = \Person.age
+
+let keyPathList = [keyPathName, keyPathAge]
+
+for keyPath in keyPathList {
+    print(person2[keyPath: keyPath])
+}
+
+
+// MARK: - AnyKeyPath
+let classKeyPathName = \AnotherPerson.name
+let classKeyPathAge = \AnotherPerson.age
+
+let keyPathList2 = [keyPathName, keyPathAge, classKeyPathName, classKeyPathAge]
+
+for keyPath in keyPathList2 {
+    if let keyPath = keyPath as? PartialKeyPath<Person> {
+        print(person1[keyPath: keyPath])
+    } else if let keyPath = keyPath as? PartialKeyPath<AnotherPerson> {
+        print(another[keyPath: keyPath])
     }
 }
