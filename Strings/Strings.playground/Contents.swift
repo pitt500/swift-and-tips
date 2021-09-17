52d9b7cfb4d0a2d68899502548f0a6f94cf37d15
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-09-17 12:53:28 -0700

    Add String folder

---
+++ Strings/Strings.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+
+var greeting: String = "Hello, playground"
+
+
+
+let escapingSpecialCharacters = #"Line 1\nLine 2"#
+print(escapingSpecialCharacters)
+print(#"Line 1\nLine 2"#)
+
+
