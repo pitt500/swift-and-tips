3e541476f620ff3150c10cf41c22a63dfbb4b35a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-09-27 06:22:05 -0700

    Remove example with int subscript

--- Strings/Strings.playground/Contents.swift
+++ Strings/Strings.playground/Contents.swift
@@ -29,8 +29,49 @@ func isPalindrome(_ s: String) -> Bool {
 }
 
 
+// MARK: - Workaround with Array of characters
 
-// Better version:
+func isPalindromeWorkaround(_ s: String) -> Bool {
+    let string = s
+        .filter { $0.isNumber || $0.isLetter }
+        .lowercased()
+    let word = [Character](string)
+    if word.count == 0 { return true }
+
+    var startIndex = 0
+    var endIndex = word.count - 1
+
+    while startIndex < endIndex {
+
+        if word[startIndex] != word[endIndex] {
+            return false
+        }
+
+        startIndex += 1
+        endIndex -= 1
+    }
+
+    return true
+}
+
+print(isPalindromeWorkaround("maam"))
+
+
+
+
+
+// MARK: - Characters in a String
+let string = "Pizza 🍕"
+for character in string {
+    print(type(of: character))
+}
+
+
+
+
+
+
+// MARK: - Better version of isPalindrome:
 func isPalindrome2(_ s: String) -> Bool {
     let word = s
         .filter { $0.isNumber || $0.isLetter }
@@ -52,4 +93,29 @@ func isPalindrome2(_ s: String) -> Bool {
     return true
 }
 
-print(isPalindrome2("Ma, a, a    m"))
+print(isPalindrome2("Ma, 1, a    m"))
+
+
+let letterE: Character = "\u{65}"
+let acute: Character = "\u{301}"
+let combinedEAcute: Character = "\u{65}\u{301}"
+
+
+
+let man = "\u{1F468}"
+let woman = "\u{1F469}"
+let boy = "\u{1f466}"
+let girl = "\u{1F467}"
+let zeroWidthJoiner = "\u{200D}"
+let family = "\u{1F468}\u{200D}\u{1F469}\u{200D}\u{1f467}\u{200D}\u{1F466}"
+
+let family2 = "👨‍👩‍👧‍👦"
+let scalars = Array(family2.unicodeScalars).map { $0.value }
+
+print(scalars)
+
+
+
+
+let numberOfCharacters = "Hello 🌎".count
+print(numberOfCharacters)
