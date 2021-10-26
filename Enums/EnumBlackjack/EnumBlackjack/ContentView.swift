0fc494a02ef05ec4f1efb7102b2efa2f064d07fe
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-10-26 06:47:13 -0700

    Add EnumBlackjack demo

---
+++ Enums/EnumBlackjack/EnumBlackjack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnumBlackjack
+//
+//  Created by Pedro Rojas on 26/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Enums/EnumBlackjack/EnumBlackjack/EnumBlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumBlackjackApp.swift
+//  EnumBlackjack
+//
+//  Created by Pedro Rojas on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct EnumBlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
