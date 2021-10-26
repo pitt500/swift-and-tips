05944d457341d2771a3c9a4e253ce51f6699e18f
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-10-26 07:33:53 -0700

    Create Card from 1 to 10

--- Enums/EnumBlackjack/EnumBlackjack/CardView.swift
+++ Enums/EnumBlackjack/EnumBlackjack/CardView.swift
@@ -8,12 +8,197 @@
 import SwiftUI
 
 struct CardView: View {
+    let card: BlackJackCard
 
     var body: some View {
-        Rectangle()
-            .fill(.white)
-            .cornerRadius(10)
-            .shadow(radius: 10)
+        ZStack {
+            Rectangle()
+                .fill(.white)
+                .cornerRadius(10)
+                .shadow(radius: 10)
+            drawCard()
+                .padding(20)
+        }
+    }
+
+    @ViewBuilder func drawCard() -> some View {
+        switch card.number {
+        case .one:
+            Text(card.suit.rawValue)
+                .font(.system(size: 90))
+        case .two:
+            VStack {
+                Text(card.suit.rawValue)
+                Spacer()
+                Text(card.suit.rawValue)
+            }
+            .font(.system(size: 90))
+        case .three:
+            VStack {
+                Text(card.suit.rawValue)
+                Spacer()
+                Text(card.suit.rawValue)
+                Spacer()
+                Text(card.suit.rawValue)
+            }
+            .font(.system(size: 90))
+        case .four:
+            VStack {
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .five:
+            VStack {
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                Text(card.suit.rawValue)
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .six:
+            VStack {
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .seven:
+            VStack {
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                Text(card.suit.rawValue)
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .eight:
+            VStack {
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                Text(card.suit.rawValue)
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Spacer()
+                Text(card.suit.rawValue)
+                Spacer()
+                HStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .nine:
+            HStack {
+                VStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                Text(card.suit.rawValue)
+                VStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        case .ten:
+            HStack {
+                VStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+                VStack {
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                }
+                VStack {
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                    Spacer()
+                    Text(card.suit.rawValue)
+                }
+            }
+            .font(.system(size: 90))
+        default:
+            Text(card.suit.rawValue)
+                .font(.system(size: 70))
+        }
     }
 }
 
@@ -22,7 +207,12 @@ struct CardView_Previews: PreviewProvider {
         ZStack {
             Color.green
                 .ignoresSafeArea()
-            CardView()
+            CardView(
+                card: BlackJackCard(
+                    suit: .heart,
+                    number: .ten
+                )
+            )
                 .padding()
         }
     }
