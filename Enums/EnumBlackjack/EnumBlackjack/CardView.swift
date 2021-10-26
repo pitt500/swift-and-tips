62752569667f10a5e6f2a8a8e414e4dc9f6f2cf0
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-10-26 08:10:11 -0700

    Add card images for jack, queen and king

--- Enums/EnumBlackjack/EnumBlackjack/CardView.swift
+++ Enums/EnumBlackjack/EnumBlackjack/CardView.swift
@@ -16,12 +16,18 @@ struct CardView: View {
                 .fill(.white)
                 .cornerRadius(10)
                 .shadow(radius: 10)
-            drawCard()
-                .padding(20)
+            GeometryReader {
+                drawCard(proxy: $0)
+                    .frame(
+                        width: $0.size.width,
+                        height: $0.size.height
+                    )
+            }
+            .padding(20)
         }
     }
 
-    @ViewBuilder func drawCard() -> some View {
+    @ViewBuilder func drawCard(proxy: GeometryProxy) -> some View {
         switch card.number {
         case .one:
             Text(card.suit.rawValue)
@@ -195,9 +201,46 @@ struct CardView: View {
                 }
             }
             .font(.system(size: 90))
-        default:
-            Text(card.suit.rawValue)
-                .font(.system(size: 70))
+        case .jack:
+            VStack {
+                Spacer()
+                Image("jack_club")
+                    .resizable()
+                    .frame(
+                        width: proxy.size.width,
+                        height: proxy.size.height*0.8)
+                    .scaleEffect(1.3)
+                Spacer()
+            }
+        case .queen:
+            VStack {
+                Spacer()
+                Image("queen_club")
+                    .resizable()
+                    .frame(
+                        width: proxy.size.width,
+                        height: proxy.size.height*0.8)
+                    .scaleEffect(1.3)
+                Spacer()
+            }
+        case .king:
+            VStack {
+                Spacer()
+                Image("king_club")
+                    .resizable()
+                    .frame(
+                        width: proxy.size.width,
+                        height: proxy.size.height*0.8)
+                    .scaleEffect(1.3)
+                Spacer()
+            }
+        case .ace:
+            VStack {
+                Spacer()
+                Text(card.suit.rawValue)
+                    .font(.system(size: 250))
+                Spacer()
+            }
         }
     }
 }
@@ -210,7 +253,7 @@ struct CardView_Previews: PreviewProvider {
             CardView(
                 card: BlackJackCard(
                     suit: .heart,
-                    number: .ten
+                    number: .ace
                 )
             )
                 .padding()
