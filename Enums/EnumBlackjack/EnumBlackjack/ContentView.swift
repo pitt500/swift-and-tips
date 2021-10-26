447185c7d2946c6a62e0a121856b34b96eb7977d
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-10-26 06:47:41 -0700

    Add EnumBlackjack demo

--- Enums/EnumBlackjack/EnumBlackjack/ContentView.swift
+++ Enums/EnumBlackjack/EnumBlackjack/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BlackJackView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Color.green
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BlackJackView()
     }
 }
