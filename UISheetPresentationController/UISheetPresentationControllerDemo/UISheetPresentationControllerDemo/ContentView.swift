defb8c1f068fa178c5c48133d5a0cb94e23a4dd5
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-11-19 07:35:00 -0700

    Add UISheetPresentationController project

---
+++ UISheetPresentationController/UISheetPresentationControllerDemo/UISheetPresentationControllerDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UISheetPresentationControllerDemo
+//
+//  Created by Pedro Rojas on 19/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UISheetPresentationController/UISheetPresentationControllerDemo/UISheetPresentationControllerDemo/UISheetPresentationControllerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISheetPresentationControllerDemoApp.swift
+//  UISheetPresentationControllerDemo
+//
+//  Created by Pedro Rojas on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UISheetPresentationControllerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
