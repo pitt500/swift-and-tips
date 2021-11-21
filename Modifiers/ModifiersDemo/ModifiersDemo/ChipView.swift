af7249dee57efc74752991c0b508f40750348887
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-11-21 09:09:27 -0700

    Update modifiers to text

---
+++ Modifiers/ModifiersDemo/ModifiersDemo/ChipView.swift
@@ -0,0 +1,20 @@
+//
+//  ChipView.swift
+//  ModifiersDemo
+//
+//  Created by Pedro Rojas on 21/11/21.
+//
+
+import SwiftUI
+
+struct ChipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChipView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChipView()
+    }
+}
--- Modifiers/ModifiersDemo/ModifiersDemo/ContentView.swift
+++ Modifiers/ModifiersDemo/ModifiersDemo/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .font(.largeTitle)
+            .foregroundColor(.white)
             .padding()
+            .background(.blue)
+
     }
 }
 
