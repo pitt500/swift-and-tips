479433f93fb8819d3b760244589eba99fd1d0d81
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-11-21 07:50:29 -0700

    Add modifier demo

---
+++ Modifiers/ModifiersDemo/ModifiersDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ModifiersDemo
+//
+//  Created by Pedro Rojas on 21/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modifiers/ModifiersDemo/ModifiersDemo/ModifiersDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModifiersDemoApp.swift
+//  ModifiersDemo
+//
+//  Created by Pedro Rojas on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ModifiersDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
