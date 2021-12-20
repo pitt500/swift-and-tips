b8384f2e48dc87e787938ea53040a7a66df577ed
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-12-20 07:26:52 -0700

    Add Drag Gesture demo

---
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DragGesture_SwiftUI
+//
+//  Created by Pedro Rojas on 20/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/DragGesture_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGesture_SwiftUIApp.swift
+//  DragGesture_SwiftUI
+//
+//  Created by Pedro Rojas on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DragGesture_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
