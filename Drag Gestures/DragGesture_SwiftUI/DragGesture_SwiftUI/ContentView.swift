db0c78b785ef37de4e96dad07443f94a667683b2
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-12-20 07:46:22 -0700

    Add circle with drag gesture

--- Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/ContentView.swift
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var location = CGPoint(x: 50, y: 50)
+
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                self.location = value.location
+            }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Circle()
+            .fill(.red)
+            .frame(width: 100, height: 100)
+            .position(location)
+            .gesture(drag)
     }
 }
 
