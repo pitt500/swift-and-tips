40988f3f2114a94b37712c15056e55385acb5a57
Pedro Rojas <pedro.rojaslo50@gmail.com>
2021-12-20 08:04:41 -0700

    Remove jump when drag starts

--- Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/ContentView.swift
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/ContentView.swift
@@ -9,20 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var location = CGPoint(x: 50, y: 50)
+    @GestureState private var startLocation: CGPoint?
 
     var drag: some Gesture {
         DragGesture()
             .onChanged { value in
-                self.location = value.location
+                var newLocation = startLocation ?? location
+                newLocation.x += value.translation.width
+                newLocation.y += value.translation.height
+
+                self.location = newLocation
+            }
+            .updating(self.$startLocation) { value, startLocation, transaction in
+                startLocation = startLocation ?? location
             }
     }
 
     var body: some View {
-        Circle()
-            .fill(.red)
-            .frame(width: 100, height: 100)
-            .position(location)
-            .gesture(drag)
+        ZStack {
+            VStack {
+                Color.white
+                Color.green
+            }
+            Circle()
+                .fill(.red)
+                .frame(width: 100, height: 100)
+                .position(location)
+                .gesture(drag)
+        }
     }
 }
 
