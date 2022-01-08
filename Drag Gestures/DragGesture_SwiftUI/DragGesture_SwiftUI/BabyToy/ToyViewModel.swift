a241bf76c09c42111b33817df33f67dcae02433c
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-08 13:13:28 -0700

    Add ToyViewModel with methods to track dragging progress

--- Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/BabyToy/ToyViewModel.swift
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/BabyToy/ToyViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Pedro Rojas on 08/01/22.
 //
 
-import Foundation
+import SwiftUI
+
+class ToyViewModel: ObservableObject {
+    @Published var isDragged = false
+    @Published var highlighedId: Int?
+    @Published var selectedId: Int?
+
+    private var frames: [Int: CGRect] = [:]
+
+    func update(frame: CGRect, for id: Int) {
+        frames[id] = frame
+    }
+
+    func update(dragLocation: CGPoint) {
+        for (id, frame) in frames where frame.contains(dragLocation) {
+            highlighedId = id
+            return
+        }
+
+        highlighedId = nil
+    }
+
+    func update(isDragged: Bool){
+        self.isDragged = isDragged
+
+        if isDragged == false, let highlighedId = self.highlighedId {
+            selectedId = id
+            self.highlighedId = nil
+        }
+    }
+
+}
