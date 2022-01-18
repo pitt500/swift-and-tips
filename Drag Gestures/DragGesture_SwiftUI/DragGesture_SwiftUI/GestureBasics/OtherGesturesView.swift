24f0ebe6a05a4dffc1224afeb330b1eec5cb4ace
Pedro Rojas <projas@Macbook-pro-M1-Max-14.local>
2022-01-18 16:42:55 -0700

    Remove DS_Store

---
+++ Drag Gestures/DragGesture_SwiftUI/DragGesture_SwiftUI/GestureBasics/OtherGesturesView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherGesturesView.swift
+//  DragGesture_SwiftUI
+//
+//  Created by Pedro Rojas on 15/01/22.
+//
+
+import SwiftUI
+
+struct OtherGesturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OtherGesturesView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherGesturesView()
+    }
+}
