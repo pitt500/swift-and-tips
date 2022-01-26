d9e0206c60fe5195e18c802ebe088ac2fb11b799
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-26 11:17:37 -0700

    Add Tap Gesture Demo

---
+++ TapGestureDemo/TapGestureDemo/TapGestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGestureDemoApp.swift
+//  TapGestureDemo
+//
+//  Created by Pedro Rojas on 26/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TapGestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
