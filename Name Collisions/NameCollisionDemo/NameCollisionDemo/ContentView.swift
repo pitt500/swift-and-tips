0a5de46b36b3a9f7e50bf4aa560b5ddf3d1d9df3
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-01-31 17:48:09 -0700

    Add Xcode Project with name collision demo

---
+++ Name Collisions/NameCollisionDemo/NameCollisionDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameCollisionDemo
+//
+//  Created by Pedro Rojas on 31/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Name Collisions/NameCollisionDemo/NameCollisionDemo/NameCollisionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameCollisionDemoApp.swift
+//  NameCollisionDemo
+//
+//  Created by Pedro Rojas on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct NameCollisionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
