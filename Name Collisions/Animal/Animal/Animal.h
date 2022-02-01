85b041c775ebc2b18f720b386ab93686902d8e7d
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-01 06:01:39 -0700

    Add Animal Framework

---
+++ Name Collisions/Animal/Animal/Animal.h
@@ -0,0 +1,18 @@
+//
+//  Animal.h
+//  Animal
+//
+//  Created by Pedro Rojas on 01/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Animal.
+FOUNDATION_EXPORT double AnimalVersionNumber;
+
+//! Project version string for Animal.
+FOUNDATION_EXPORT const unsigned char AnimalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animal/PublicHeader.h>
+
+
---
+++ Name Collisions/Animal/Animal/Dog.swift
@@ -0,0 +1,22 @@
+//
+//  Action.swift
+//  Animal
+//
+//  Created by Pedro Rojas on 01/02/22.
+//
+
+import Foundation
+
+class Dog {
+    func move() {
+        print("🐾🐾🐾🐾")
+    }
+    
+    func run() {
+        print("🦴🦮")
+    }
+    
+    func sleep() {
+        print("😴")
+    }
+}
