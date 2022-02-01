fd48fd0b3baf918c57bcbfe03b4c869a4f4dc93a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-01 06:38:17 -0700

    Add Human Framework to the workspace

---
+++ Name Collisions/Human/Human/Human.h
@@ -0,0 +1,18 @@
+//
+//  Human.h
+//  Human
+//
+//  Created by Pedro Rojas on 01/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Human.
+FOUNDATION_EXPORT double HumanVersionNumber;
+
+//! Project version string for Human.
+FOUNDATION_EXPORT const unsigned char HumanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Human/PublicHeader.h>
+
+
