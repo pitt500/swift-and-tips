d084c45ff6c402b37b8ed00ec79610d3bf78c578
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-04 08:20:51 -0700

    Add Magnification Gesture Project

---
+++ MagnificationGestureDemo/MagnificationGestureDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagnificationGestureDemo
+//
+//  Created by Pedro Rojas on 04/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MagnificationGestureDemo/MagnificationGestureDemo/MagnificationGestureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnificationGestureDemoApp.swift
+//  MagnificationGestureDemo
+//
+//  Created by Pedro Rojas on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MagnificationGestureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
