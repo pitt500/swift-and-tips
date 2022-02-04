625f45ec8e1f959be66e86cf6ebd9ef07154e98a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-04 13:14:24 -0700

    Add magnification to detail view

--- MagnificationGestureDemo/MagnificationGestureDemo/GalleryView.swift
+++ MagnificationGestureDemo/MagnificationGestureDemo/GalleryView.swift
@@ -32,7 +32,7 @@ struct GalleryView: View {
                     }
                 }
             }
-            .navigationTitle("Soccer")
+            .navigationTitle("Gallery")
         }
     }
 }
--- MagnificationGestureDemo/MagnificationGestureDemo/ImageDetailView.swift
+++ MagnificationGestureDemo/MagnificationGestureDemo/ImageDetailView.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct ImageDetailView: View {
     let image: Image
+    @State private var scale = 1.0
+    
+    var magnification: some Gesture {
+        MagnificationGesture()
+            .onChanged { delta in
+                scale *= delta
+                print("onChanged: \(scale)")
+            }
+            .onEnded { state in
+                print("onEnded: \(scale)")
+                withAnimation {
+                    scale = max(scale, 1.0)
+                }
+            }
+    }
     
     var body: some View {
         image
             .resizable()
             .aspectRatio(contentMode: .fit)
+            .scaleEffect(scale)
+            .gesture(magnification)
     }
 }
 
