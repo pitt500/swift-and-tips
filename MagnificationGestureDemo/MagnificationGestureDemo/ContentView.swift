28334cbb2d440b1851c10f15423505a98b575340
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-04 08:56:49 -0700

    Add preferred dark scheme to preview

--- MagnificationGestureDemo/MagnificationGestureDemo/ContentView.swift
+++ MagnificationGestureDemo/MagnificationGestureDemo/ContentView.swift
@@ -33,5 +33,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
