b190c0cdddfd3a5aefbdbf0cb83ea338f0913d27
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-02-23 18:50:40 -0700

    Add Shapes Demo

---
+++ ShapesDemo/ShapesDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShapesDemo
+//
+//  Created by Pedro Rojas on 23/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShapesDemo/ShapesDemo/ShapesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesDemoApp.swift
+//  ShapesDemo
+//
+//  Created by Pedro Rojas on 23/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
